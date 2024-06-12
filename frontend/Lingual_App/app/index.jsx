8b14b677a23f747016206bb41c9ab44aebf15c57
Koketjo Mamashela <kokimamashela@gmail.com>
2024-06-12 12:04:45 +0200

    Added tailwindcss for styling

--- frontend/Lingual_App/app/index.jsx
+++ frontend/Lingual_App/app/index.jsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
@@ -12,3 +13,11 @@ export default function App() {
     );
 }
 
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
