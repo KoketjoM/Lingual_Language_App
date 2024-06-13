f004e328dabc6aaab11293b05f575d1afb1008aa
Koketjo Mamashela <kokimamashela@gmail.com>
2024-06-13 14:03:41 +0200

    Updated Tailwind colours and fonts

--- frontend/Lingual_App/app/index.jsx
+++ frontend/Lingual_App/app/index.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
+
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-2xl">Lingual!</Text>
+            <Text className="text-2xl font-pbold" >Lingual</Text>
             <StatusBar style="auto" />
             <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
         </View>
--- frontend/Lingual_App/tailwind.config.js
+++ frontend/Lingual_App/tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FE504F', //off white 
+        secondary: {
+          DEFAULT: '#FE504F', //creamcicle red
+          100: '#FE7372' //dark mode version
+        },
+        firstAccent: {
+          DEFAULT: '#FF9841', //creamcicle orange
+          100: '#FFAD67' //dark mode version
+        },
+        secondAccent: {
+          DEFAULT: '#004DFF', //vibrant blue
+          100: '#437CFF' //dark mode version
+        },
+        thirdAccent: {
+          DEFAULT: '#A201FF', // grape purple
+          100: '#BE4CFF' //dark mode version
+        },
+        black: '#000',
+        white: '#fff',
+        lightGrey: '#ADB3BC',
+        darkGrey: '#455370'
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
